44ad0d7e347f3e5dbd369cb801b1242c99a63f21
Tostka <tostka@users.noreply.github.com>
2020-05-01 16:26:33 -0500

    verb-mods v102
    4:26 PM 5/1/2020

---
+++ Public/load-ModuleFT.ps1
@@ -0,0 +1,129 @@
+#*------v Function load-ModuleFT v------
+function load-ModuleFT {
+    <#
+    .SYNOPSIS
+    load-ModuleFT - Import-Module, with fault-tolerant coverage, when not available to load as normal module.
+    .NOTES
+    Version     : 1.0.0
+    Author      : Todd Kadrie
+    Website     : http://www.toddomation.com
+    Twitter     : @tostka / http://twitter.com/tostka
+    CreatedDate : 2020-04-29
+    FileName    : 
+    License     : MIT License
+    Copyright   : (c) 2020 Todd Kadrie
+    Github      : https://github.com/tostka
+    AddedCredit : REFERENCE
+    REVISIONS
+    * 2:57 PM 4/29/2020 port from code in use in .ps1's & modules
+    .DESCRIPTION
+    load-ModuleFT - Import-Module, with fault-tolerant coverage, when not available to load as normal module.
+    .PARAMETER  tModName
+    Module name to be loaded or installed [ -tModName Azure]
+    .PARAMETER ParentPath
+    Calling script path (used for log construction)[-ParentPath c:\pathto\script.ps1]
+    .PARAMETER LoggingOn
+    Initiate logging Flag [-LoggingOn]
+    .PARAMETER ShowDebug
+    Parameter to display Debugging messages [-ShowDebug switch]
+    .PARAMETER Whatif
+    Parameter to run a Test no-change pass [-Whatif switch]
+    .OUTPUT
+    Returns an object with properties:
+    [boolean]ModStatus ($tModmdlet validated avail) ; 
+    [boolean]Logging ; 
+    [string]$logfile ; 
+    [string]$transcript
+    .EXAMPLE
+    load-ModuleFT -tModName verb-Azure -tModFile C:\sc\verb-Azure\verb-Azure\verb-Azure.psm1 -tModCmdlet get-AADBearToken ; 
+    .EXAMPLE
+    $tMod="verb-Azure;C:\sc\verb-Azure\verb-Azure\verb-Azure.psm1;get-AADBearToken" ;
+    load-ModuleFT -tModName $tMod.split(';')[0] -tModFile $tMod.split(';')[1] -tModCmdlet $tMod.split(';')[2] ; 
+    .LINK
+    https://github.com/tostka
+    #>
+    #  $tModName = $tMod.split(';')[0] ; $tModFile = $tMod.split(';')[1] ; $tModCmdlet = $tMod.split(';')[2] ; 
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory = $True,HelpMessage = "Name of Module name to be loaded [ -tModName Azure]")]
+        [ValidateNotNullOrEmpty()][string]$tModName,
+        [Parameter(Mandatory = $True,HelpMessage = "Path to xxx.psm1 source file for module to be loaded [ -tModName C:\sc\verb-Azure\verb-Azure\verb-Azure.psm1]")]
+        [ValidateNotNullOrEmpty()][string]$tModFile,
+        [Parameter(Mandatory = $True,HelpMessage = "Cmdlet to be validated present for module to be loaded [-tModCmdlet get-AADBearToken]")]
+        [ValidateNotNullOrEmpty()][string]$tModCmdlet,
+        [Parameter(HelpMessage = "Debugging Flag [-showDebug]")]
+        [switch] $showDebug,
+        [Parameter(HelpMessage = "Whatif Flag  [-whatIf]")]
+        [switch] $whatIf
+    ) ;
+    BEGIN{
+        # Get the name of this function
+        ${CmdletName} = $PSCmdlet.MyInvocation.MyCommand.Name ;
+        # Get parameters this function was invoked with
+        $PSParameters = New-Object -TypeName PSObject -Property $PSBoundParameters ;
+        $Verbose = ($VerbosePreference -eq "Continue") ; 
+    } ; 
+    PROCESS{
+        $smsg = "( processing `$tModName:$($tModName)`t`$tModFile:$($tModFile)`t`$tModCmdlet:$($tModCmdlet) )" ; 
+        if ($logging) { Write-Log -LogContent $smsg -Path $logfile -useHost -Level Info } #Error|Warn|Debug 
+        else{ write-host -foregroundcolor green "$((get-date).ToString('HH:mm:ss')):$($smsg)" } ; 
+        if($tModName -eq 'verb-logging' -OR $tModName -eq 'verb-Azure'){
+            write-host "GOTCHA!" ;
+        } ;
+        $lVers = get-module -name $tModName -ListAvailable -ea 0 ;
+        if($lVers){
+            $lVers=($lVers | sort version)[-1];
+            try {
+                import-module -name $tModName -RequiredVersion $lVers.Version.tostring() -force -DisableNameChecking   
+            } catch {
+                write-warning "*BROKEN INSTALLED MODULE*:$($tModName)`nBACK-LOADING DCOPY@ $($tModDFile)" ;
+                import-module -name $tModDFile -force -DisableNameChecking   
+            } ;
+        } elseif (test-path $tModFile) {
+            write-warning "*NO* INSTALLED MODULE*:$($tModName)`nBACK-LOADING DCOPY@ $($tModDFile)" ;
+            try {
+                import-module -name $tModDFile -force -DisableNameChecking
+            } catch {
+                write-error "*FAILED* TO LOAD MODULE*:$($tModName) VIA $(tModFile) !" ;
+                $tModFile = "$($tModName).ps1" ;
+                $sLoad = (join-path -path $LocalInclDir -childpath $tModFile) ;
+                if (Test-Path $sLoad) {       Write-Verbose -verbose ((Get-Date).ToString("HH:mm:ss") + "LOADING:" + $sLoad) ;
+                   . $sLoad ;
+                   if ($showdebug) { Write-Verbose -verbose "Post $sLoad" };
+                } else {
+                    $sLoad = (join-path -path $backInclDir -childpath $tModFile) ;
+                    if (Test-Path $sLoad) {
+                    
+                        Write-Verbose -verbose ((Get-Date).ToString("HH:mm:ss") + "LOADING:" + $sLoad) ;
+                        . $sLoad ;
+                        if ($showdebug) { Write-Verbose -verbose "Post $sLoad" } }
+                        else {
+                            Write-Warning ((Get-Date).ToString("HH:mm:ss") + ":MISSING:" + $sLoad + " EXITING...") ;           exit;       
+                        } ;
+                } ; 
+            } ; 
+        } ;
+        if(!(test-path function:$tModCmdlet)){
+            write-warning -verbose:$true  "UNABLE TO VALIDATE PRESENCE OF $tModCmdlet`nfailing through to `$backInclDir .ps1 version" ;
+            $sLoad = (join-path -path $backInclDir -childpath "$($tModName).ps1") ;
+            if (Test-Path $sLoad) {
+                Write-Verbose -verbose:$true ((Get-Date).ToString("HH:mm:ss") + "LOADING:" + $sLoad) ;
+                . $sLoad ;
+                if ($showdebug) { Write-Verbose -verbose "Post $sLoad" };
+                if(!(test-path function:$tModCmdlet)){
+                    write-warning "$((get-date).ToString('HH:mm:ss')):FAILED TO CONFIRM `$tModCmdlet:$($tModCmdlet) FOR $($tModName)" ;
+                } else {write-verbose -verbose:$true  "(confirmed $tModName loaded: $tModCmdlet present)"} ;   
+            } else {
+                Write-Warning ((Get-Date).ToString("HH:mm:ss") + ":MISSING:" + $sLoad + " EXITING...") ;
+                $ModStatus = $false ; 
+                exit;
+            } ; 
+        } else {     
+            write-verbose -verbose:$true  "(confirmed $tModName loaded: $tModCmdlet present)" ; 
+            $ModStatus = $true ; 
+        } ; 
+    } ;  # PROC-E
+    END {
+        $ModStatus | write-output ;
+    } ; 
+} #*------^ END Function load-ModuleFT ^------ ;
--- verb-Mods/verb-Mods.psm1
+++ verb-Mods/verb-Mods.psm1
@@ -1,10 +1,11 @@
 ﻿# verb-Mods.psm1
 
+
   <#
   .SYNOPSIS
   verb-Mods - Generic module-related functions
   .NOTES
-  Version     : 1.0.1.0
+  Version     : 1.0.2.0
   Author      : Todd Kadrie
   Website     :	https://www.toddomation.com
   Twitter     :	@tostka
@@ -24,8 +25,10 @@
   #>
 
 
-$script:ModuleRoot = $PSScriptRoot ; 
-$script:ModuleVersion = (Import-PowerShellDataFile -Path (get-childitem $script:moduleroot\*.psd1).fullname).moduleversion ; 
+$script:ModuleRoot = $PSScriptRoot ;
+$script:ModuleVersion = (Import-PowerShellDataFile -Path (get-childitem $script:moduleroot\*.psd1).fullname).moduleversion ;
+
+#*======v FUNCTIONS v======
 
 
 
@@ -35,7 +38,7 @@ function check-ReqMods {
     .SYNOPSIS
     check-ReqMods() - Verifies that specified commands exist in function: (are loaded) or get-command (registered via installed .psd modules)
     .NOTES
-    Version     : 1.0.1
+    Version     : 1.0.0
     Author      : Todd Kadrie
     Website     : http://www.toddomation.com
     Twitter     : @tostka / http://twitter.com/tostka
@@ -87,7 +90,7 @@ Function Disconnect-PssBroken {
     .SYNOPSIS
     Disconnect-PssBroken - Remove all local broken PSSessions
     .NOTES
-    Version     : 1.0.1
+    Version     : 1.0.0
     Author      : Todd Kadrie
     Website     :	http://www.toddomation.com
     Twitter     :	@tostka / http://twitter.com/tostka
@@ -116,16 +119,217 @@ Function Disconnect-PssBroken {
 
 #*------^ Disconnect-PssBroken.ps1 ^------
 
+#*------v load-Module.ps1 v------
+function load-Module {
+    <#
+    .SYNOPSIS
+    load-Module - Import-Module, with Find- & Install-, when not available to load.
+    .NOTES
+    Version     : 1.0.0
+    Author      : Todd Kadrie
+    Website     : http://www.toddomation.com
+    Twitter     : @tostka / http://twitter.com/tostka
+    CreatedDate : 2019-8-28
+    FileName    : 
+    License     : MIT License
+    Copyright   : (c) 2020 Todd Kadrie
+    Github      : https://github.com/tostka
+    AddedCredit : REFERENCE
+    AddedWebsite:	URL
+    AddedTwitter:	URL
+    REVISIONS
+    * 7:29 AM 1/29/2020 added pshelp, version etc (copying into verb-dev)
+    * 8/28/2019 init
+    .DESCRIPTION
+    load-Module - Import-Module, with Find- & Install-, when not available to load.
+    .PARAMETER  Module
+    Module name to be loaded or installed [ -Module Azure]
+    .PARAMETER ShowDebug
+    Parameter to display Debugging messages [-ShowDebug switch]
+    .PARAMETER Whatif
+    Parameter to run a Test no-change pass [-Whatif switch]
+    .EXAMPLE
+    .\load-Module Azure
+    .LINK
+    https://github.com/tostka
+    #>
+    [CmdletBinding()]
+    Param(
+        [Parameter(Position = 0, Mandatory = $True, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true, HelpMessage = "Module name to be loaded or installed [ -Module Azure]")]
+        [ValidateNotNullOrEmpty()][string]$Module,
+        [Parameter(HelpMessage = "Debugging Flag [-showDebug]")]
+        [switch] $showDebug,
+        [Parameter(HelpMessage = "Whatif Flag  [-whatIf]")]
+        [switch] $whatIf
+    ) ;
+    $Verbose = ($VerbosePreference -eq "Continue") ; 
+    if!(Get-Module -Name $Module){
+        if (Get-Module -Name $Module -ListAvailable) {
+            Import-Module $Module ;
+        } else {
+            write-host -foregroundcolor RED "$((get-date).ToString('HH:mm:ss')):ERROR!:The $($Module) module is *NOT* INSTALLED!.`n Checking for available copy..." ;
+            if(find-module $Module){
+                write-host -foregroundcolor green "$((get-date).ToString('HH:mm:ss')):Would you *LIKE* to install the $Module module *NOW*?" ;
+                $bRet = Read-Host "Enter YYY to continue. Anything else will exit"
+                if ($bRet.ToUpper() -eq "YYY") {
+                    Write-host "Installing Module:$($Module)`nInstall-Module -Name $($Module) -AllowClobber -Scope CurrentUser..."
+                    Install-Module -Name $Module -AllowClobber -Scope CurrentUser
+                } else {
+                    write-host -foregroundcolor green "$((get-date).ToString('HH:mm:ss')):Install declined. Aborting script pass.`nThe required $($Module) module can be installed via the`nInstall-Module -Name $($Module) -AllowClobber -Scope CurrentUser`ncommand.`nEXITING"
+                    # exit <asserted exit error #>
+                    exit 1
+                } # if-block end
+            } else {
+                write-host -foregroundcolor RED "$((get-date).ToString('HH:mm:ss')):ERROR!:The $($Module) module was not found at the routine Repositories. `nPlease locate a copy and install it before attempting to use this script" ;
+            } ;
+        } ;
+    } ;
+}
+
+#*------^ load-Module.ps1 ^------
+
+#*------v load-ModuleFT.ps1 v------
+function load-ModuleFT {
+    <#
+    .SYNOPSIS
+    load-ModuleFT - Import-Module, with fault-tolerant coverage, when not available to load as normal module.
+    .NOTES
+    Version     : 1.0.0
+    Author      : Todd Kadrie
+    Website     : http://www.toddomation.com
+    Twitter     : @tostka / http://twitter.com/tostka
+    CreatedDate : 2020-04-29
+    FileName    : 
+    License     : MIT License
+    Copyright   : (c) 2020 Todd Kadrie
+    Github      : https://github.com/tostka
+    AddedCredit : REFERENCE
+    REVISIONS
+    * 2:57 PM 4/29/2020 port from code in use in .ps1's & modules
+    .DESCRIPTION
+    load-ModuleFT - Import-Module, with fault-tolerant coverage, when not available to load as normal module.
+    .PARAMETER  tModName
+    Module name to be loaded or installed [ -tModName Azure]
+    .PARAMETER ParentPath
+    Calling script path (used for log construction)[-ParentPath c:\pathto\script.ps1]
+    .PARAMETER LoggingOn
+    Initiate logging Flag [-LoggingOn]
+    .PARAMETER ShowDebug
+    Parameter to display Debugging messages [-ShowDebug switch]
+    .PARAMETER Whatif
+    Parameter to run a Test no-change pass [-Whatif switch]
+    .OUTPUT
+    Returns an object with properties:
+    [boolean]ModStatus ($tModmdlet validated avail) ; 
+    [boolean]Logging ; 
+    [string]$logfile ; 
+    [string]$transcript
+    .EXAMPLE
+    load-ModuleFT -tModName verb-Azure -tModFile C:\sc\verb-Azure\verb-Azure\verb-Azure.psm1 -tModCmdlet get-AADBearToken ; 
+    .EXAMPLE
+    $tMod="verb-Azure;C:\sc\verb-Azure\verb-Azure\verb-Azure.psm1;get-AADBearToken" ;
+    load-ModuleFT -tModName $tMod.split(';')[0] -tModFile $tMod.split(';')[1] -tModCmdlet $tMod.split(';')[2] ; 
+    .LINK
+    https://github.com/tostka
+    #>
+    #  $tModName = $tMod.split(';')[0] ; $tModFile = $tMod.split(';')[1] ; $tModCmdlet = $tMod.split(';')[2] ; 
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory = $True,HelpMessage = "Name of Module name to be loaded [ -tModName Azure]")]
+        [ValidateNotNullOrEmpty()][string]$tModName,
+        [Parameter(Mandatory = $True,HelpMessage = "Path to xxx.psm1 source file for module to be loaded [ -tModName C:\sc\verb-Azure\verb-Azure\verb-Azure.psm1]")]
+        [ValidateNotNullOrEmpty()][string]$tModFile,
+        [Parameter(Mandatory = $True,HelpMessage = "Cmdlet to be validated present for module to be loaded [-tModCmdlet get-AADBearToken]")]
+        [ValidateNotNullOrEmpty()][string]$tModCmdlet,
+        [Parameter(HelpMessage = "Debugging Flag [-showDebug]")]
+        [switch] $showDebug,
+        [Parameter(HelpMessage = "Whatif Flag  [-whatIf]")]
+        [switch] $whatIf
+    ) ;
+    BEGIN{
+        # Get the name of this function
+        ${CmdletName} = $PSCmdlet.MyInvocation.MyCommand.Name ;
+        # Get parameters this function was invoked with
+        $PSParameters = New-Object -TypeName PSObject -Property $PSBoundParameters ;
+        $Verbose = ($VerbosePreference -eq "Continue") ; 
+    } ; 
+    PROCESS{
+        $smsg = "( processing `$tModName:$($tModName)`t`$tModFile:$($tModFile)`t`$tModCmdlet:$($tModCmdlet) )" ; 
+        if ($logging) { Write-Log -LogContent $smsg -Path $logfile -useHost -Level Info } #Error|Warn|Debug 
+        else{ write-host -foregroundcolor green "$((get-date).ToString('HH:mm:ss')):$($smsg)" } ; 
+        if($tModName -eq 'verb-logging' -OR $tModName -eq 'verb-Azure'){
+            write-host "GOTCHA!" ;
+        } ;
+        $lVers = get-module -name $tModName -ListAvailable -ea 0 ;
+        if($lVers){
+            $lVers=($lVers | sort version)[-1];
+            try {
+                import-module -name $tModName -RequiredVersion $lVers.Version.tostring() -force -DisableNameChecking   
+            } catch {
+                write-warning "*BROKEN INSTALLED MODULE*:$($tModName)`nBACK-LOADING DCOPY@ $($tModDFile)" ;
+                import-module -name $tModDFile -force -DisableNameChecking   
+            } ;
+        } elseif (test-path $tModFile) {
+            write-warning "*NO* INSTALLED MODULE*:$($tModName)`nBACK-LOADING DCOPY@ $($tModDFile)" ;
+            try {
+                import-module -name $tModDFile -force -DisableNameChecking
+            } catch {
+                write-error "*FAILED* TO LOAD MODULE*:$($tModName) VIA $(tModFile) !" ;
+                $tModFile = "$($tModName).ps1" ;
+                $sLoad = (join-path -path $LocalInclDir -childpath $tModFile) ;
+                if (Test-Path $sLoad) {       Write-Verbose -verbose ((Get-Date).ToString("HH:mm:ss") + "LOADING:" + $sLoad) ;
+                   . $sLoad ;
+                   if ($showdebug) { Write-Verbose -verbose "Post $sLoad" };
+                } else {
+                    $sLoad = (join-path -path $backInclDir -childpath $tModFile) ;
+                    if (Test-Path $sLoad) {
+                    
+                        Write-Verbose -verbose ((Get-Date).ToString("HH:mm:ss") + "LOADING:" + $sLoad) ;
+                        . $sLoad ;
+                        if ($showdebug) { Write-Verbose -verbose "Post $sLoad" } }
+                        else {
+                            Write-Warning ((Get-Date).ToString("HH:mm:ss") + ":MISSING:" + $sLoad + " EXITING...") ;           exit;       
+                        } ;
+                } ; 
+            } ; 
+        } ;
+        if(!(test-path function:$tModCmdlet)){
+            write-warning -verbose:$true  "UNABLE TO VALIDATE PRESENCE OF $tModCmdlet`nfailing through to `$backInclDir .ps1 version" ;
+            $sLoad = (join-path -path $backInclDir -childpath "$($tModName).ps1") ;
+            if (Test-Path $sLoad) {
+                Write-Verbose -verbose:$true ((Get-Date).ToString("HH:mm:ss") + "LOADING:" + $sLoad) ;
+                . $sLoad ;
+                if ($showdebug) { Write-Verbose -verbose "Post $sLoad" };
+                if(!(test-path function:$tModCmdlet)){
+                    write-warning "$((get-date).ToString('HH:mm:ss')):FAILED TO CONFIRM `$tModCmdlet:$($tModCmdlet) FOR $($tModName)" ;
+                } else {write-verbose -verbose:$true  "(confirmed $tModName loaded: $tModCmdlet present)"} ;   
+            } else {
+                Write-Warning ((Get-Date).ToString("HH:mm:ss") + ":MISSING:" + $sLoad + " EXITING...") ;
+                $ModStatus = $false ; 
+                exit;
+            } ; 
+        } else {     
+            write-verbose -verbose:$true  "(confirmed $tModName loaded: $tModCmdlet present)" ; 
+            $ModStatus = $true ; 
+        } ; 
+    } ;  # PROC-E
+    END {
+        $ModStatus | write-output ;
+    } ; 
+}
+
+#*------^ load-ModuleFT.ps1 ^------
+
 #*======^ END FUNCTIONS ^======
 
-Export-ModuleMember -Function check-ReqMods,Disconnect-PssBroken -Alias *
+Export-ModuleMember -Function check-ReqMods,Disconnect-PssBroken,load-Module,load-ModuleFT -Alias *
 
 
 # SIG # Begin signature block
 # MIIELgYJKoZIhvcNAQcCoIIEHzCCBBsCAQExCzAJBgUrDgMCGgUAMGkGCisGAQQB
 # gjcCAQSgWzBZMDQGCisGAQQBgjcCAR4wJgIDAQAABBAfzDtgWUsITrck0sYpfvNR
-# AgEAAgEAAgEAAgEAAgEAMCEwCQYFKw4DAhoFAAQUeD7bAP1bsPwyxNLt23tsLh+L
-# fyigggI4MIICNDCCAaGgAwIBAgIQWsnStFUuSIVNR8uhNSlE6TAJBgUrDgMCHQUA
+# AgEAAgEAAgEAAgEAAgEAMCEwCQYFKw4DAhoFAAQUrZcbzENcJ7cg9Jac/YuWRnsL
+# 9QSgggI4MIICNDCCAaGgAwIBAgIQWsnStFUuSIVNR8uhNSlE6TAJBgUrDgMCHQUA
 # MCwxKjAoBgNVBAMTIVBvd2VyU2hlbGwgTG9jYWwgQ2VydGlmaWNhdGUgUm9vdDAe
 # Fw0xNDEyMjkxNzA3MzNaFw0zOTEyMzEyMzU5NTlaMBUxEzARBgNVBAMTClRvZGRT
 # ZWxmSUkwgZ8wDQYJKoZIhvcNAQEBBQADgY0AMIGJAoGBALqRVt7uNweTkZZ+16QG
@@ -140,9 +344,9 @@ Export-ModuleMember -Function check-ReqMods,Disconnect-PssBroken -Alias *
 # AWAwggFcAgEBMEAwLDEqMCgGA1UEAxMhUG93ZXJTaGVsbCBMb2NhbCBDZXJ0aWZp
 # Y2F0ZSBSb290AhBaydK0VS5IhU1Hy6E1KUTpMAkGBSsOAwIaBQCgeDAYBgorBgEE
 # AYI3AgEMMQowCKACgAChAoAAMBkGCSqGSIb3DQEJAzEMBgorBgEEAYI3AgEEMBwG
-# CisGAQQBgjcCAQsxDjAMBgorBgEEAYI3AgEVMCMGCSqGSIb3DQEJBDEWBBQnc9fo
-# EuFE97R/v09GMg9RQ94NODANBgkqhkiG9w0BAQEFAASBgH8o2acKL38aMR6wVn9D
-# xlT/lmmAyprlLebUR2asEWHnS2Kt94kx6LTlhCiSC2L5ReMoOzCxrPcoy9jVf6Ud
-# jOBB6jKkIrD30L+Mwpm0TUG8/Uq45F4+3cmRv9vfOdMECnD+qa1rG6omZ8BtEf1t
-# 466zPwcnf+O3XtmBaEblbLLU
+# CisGAQQBgjcCAQsxDjAMBgorBgEEAYI3AgEVMCMGCSqGSIb3DQEJBDEWBBRmxo+U
+# iolBoB7fhrdghtP8TxoXozANBgkqhkiG9w0BAQEFAASBgH3gJ7MHFDX2vHP+jfUv
+# /09lDRY9ovJVz1Qt2XP231r3qBWFh9K5PavtBBfXOMoX3Bl/ds0URYBQJzy6FGm8
+# lbkJ5hz7S+O0goqERitiHjLgKGYKG2yFXgBQxyEg1nfSbKu3Az1e0FtLjqDfK1+d
+# eBxsVQQjmR3A3Jq7GLCgESTe
 # SIG # End signature block
