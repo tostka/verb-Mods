0a0c7d2b4d91f0bc6162b0c1d2601c31ab7e146b
Tostka <tostka@users.noreply.github.com>
2022-09-08 16:49:07 -0500

    vmods v1.2.2
    4:48 PM 9/8/2022

--- verb-Mods/verb-Mods.psm1
+++ verb-Mods/verb-Mods.psm1
@@ -1,11 +1,11 @@
-﻿# verb-Mods.psm1
+﻿# verb-mods.psm1
 
 
   <#
   .SYNOPSIS
   verb-Mods - Generic module-related functions
   .NOTES
-  Version     : 1.2.1.0
+  Version     : 1.2.2.0
   Author      : Todd Kadrie
   Website     :	https://www.toddomation.com
   Twitter     :	@tostka
@@ -25,10 +25,12 @@
   #>
 
 
-$script:ModuleRoot = $PSScriptRoot ;
-$script:ModuleVersion = (Import-PowerShellDataFile -Path (get-childitem $script:moduleroot\*.psd1).fullname).moduleversion ;
+    $script:ModuleRoot = $PSScriptRoot ;
+    $script:ModuleVersion = (Import-PowerShellDataFile -Path (get-childitem $script:moduleroot\*.psd1).fullname).moduleversion ;
+    $runningInVsCode = $env:TERM_PROGRAM -eq 'vscode' ;
 
 #*======v FUNCTIONS v======
+
 
 
 
@@ -85,6 +87,7 @@ function check-ReqMods {
 
 #*------^ check-ReqMods.ps1 ^------
 
+
 #*------v Disconnect-PssBroken.ps1 v------
 Function Disconnect-PssBroken {
     <#
@@ -119,6 +122,7 @@ Function Disconnect-PssBroken {
 }
 
 #*------^ Disconnect-PssBroken.ps1 ^------
+
 
 #*------v find-profileScripts.ps1 v------
 function find-profileScripts {
@@ -179,6 +183,7 @@ function find-profileScripts {
 }
 
 #*------^ find-profileScripts.ps1 ^------
+
 
 #*------v Get-ModulePublishedVersion.ps1 v------
 function Get-ModulePublishedVersion {
@@ -407,6 +412,7 @@ Author      : Todd Kadrie
 
 #*------^ Get-ModulePublishedVersion.ps1 ^------
 
+
 #*------v get-RepoModulesHighestVersion.ps1 v------
 function get-RepoModulesHighestVersion {
     <#
@@ -427,10 +433,12 @@ function get-RepoModulesHighestVersion {
     AddedWebsite: URL
     AddedTwitter: URL
     REVISIONS
+    * 9:40 AM 4/22/2022 fix: typo in echo
+    * 4:21 PM 4/21/2022 add: test-isUNCPath () ; PSv2-compatible PSGet:install-module supporting approach:  as PsV2 lacks PowershellGet support, this specifies the PSRepository.SourceLocation property as the -Repository input.  The script then skips the normal get-PSRepository resolution, and uses the specified UNC path, and the -Names (array of module names), to target variant revisions on the Repository.
     * 2:02 PM 11/3/2021 init, flipped to verb-mods func
     .DESCRIPTION
     get-RepoModulesHighestVersion.ps1 - Check specified Repository (defaults to $global:localPsRepo) for highest .nupkg verison for each of module directories specified by -Paths.
-    Calculates and sorts on _actual_ semantic version (rather than strict n.n.n text), to determine highest/latest revision. 
+    Calculates and sorts on _actual_ semantic version (rather than strict n.n.n text), to determine highest/latest revision.
     .PARAMETER Paths
     Module source paths to be processed[-paths 'c:\path-to\','c:\path2-to']
     .PARAMETER OutObject
@@ -441,70 +449,157 @@ function get-RepoModulesHighestVersion {
     None. Returns no objects or output (.NET types)
     .EXAMPLE
     PS> $Paths += (resolve-path c:\sc\verb*) ;
-    PS> get-RepoModulesHighestVersion -Paths $Paths -verbose ; 
+    PS> get-RepoModulesHighestVersion -Paths $Paths -verbose ;
     Run a pass against all paths below the c:\sc\ directory, with directory names starting with verb*, with verbose output.
     .EXAMPLE
-    PS> $latestRevs = (resolve-path c:\sc\verb*) | get-RepoModulesHighestVersion ; 
-    PS> $latestRevs |ft -a name,lastwritetime,version ; 
+    PS> $latestRevs = (resolve-path c:\sc\verb*) | get-RepoModulesHighestVersion ;
+    PS> $latestRevs |ft -a name,lastwritetime,version ;
     Pipeline example, process list of dirs matching pattern c:\sc\verb*, for highest .nupkg revision in the default local repository, and assign summary object to $latestRevs, then output a summary of the returned object.
+    .EXAMPLE
+    PS> $latestRevs = get-RepoModulesHighestVersion -Names verb-IO -Repository "\\lynmsv10\lync_fs\scripts\sc" -verbose ;
+    PS> $latestRevs |ft -a name,lastwritetime,version ;
+    PS> install-Module -ModulePath $latestRevs.fullname -Global
+    PSv2-compatible PSGet:install-module supporting approach:
+    - as PsV2 lacks PowershellGet support, this specifies the PSRepository.SourceLocation property as the -Repository input.
+    - The script then skips the normal get-PSRepository resolution, and uses the specified UNC path, and the -Names (array of module names), to target variant revisions on the Repository.
+    - The resulting highest semantic-version is returned and assigned to the $latestRevs variable, which is summarized in an echo back to console.
+    - Finally the $latestRev version fullname is used with the PsGet install-module command, under the -ModulePath, to install the latest version locally on a Psv2 machine.
+    .EXAMPLE
+    PS> $latestRevs = get-RepoModulesHighestVersion -Names verb-IO,verb-logging,verb-text,verb-Network -Repository ((get-psrepository -Name $global:localPSRepo).sourcelocation) -verbose ;
+    PS> $latestRevs |ft -a name,lastwritetime,version ;
+    PS> $latestRevs.fullname ;
+    PS> $latestRevs.fullname | %{write-host "==$($_):" ; install-Module -ModulePath $_ -Global -verbose ; } ;
+    PSv2-compatible PSGet:install-module supporting approach:
+    Psv2-compatible, PsGet:install-module() another simple variant that uses get-PsRepository to populate the inbound UNC path (as this verb-modules module would traditionally be run from a Psv3+ machine, with full PowershellGet support, no reason to have to hand-specify the local PsRepo SourceLocation).
+    Note: PsGet:import-module() isn't written with -ModulePath array support ([string[]]) , so we externally loop it through.
     .LINK
     https://github.com/tostka/verb-Mods
     #>
     # VALIDATORS: [ValidateNotNull()][ValidateNotNullOrEmpty()][ValidateLength(24,25)][ValidateLength(5)][ValidatePattern("some\sregex\sexpr")][ValidateSet("USEA","GBMK","AUSYD")][ValidateScript({Test-Path $_ -PathType 'Container'})][ValidateScript({Test-Path $_})][ValidateRange(21,65)][ValidateCount(1,3)]
-    [CmdletBinding()]
+    [CmdletBinding(DefaultParameterSetName='Paths')]
     PARAM(
-        [Parameter(Position=0,Mandatory=$True,ValueFromPipeline=$true,HelpMessage="Module source paths to be processed[-paths 'c:\path-to\','c:\path2-to']")]
+        [Parameter(ParameterSetName='Paths',Position=0,Mandatory=$false,ValueFromPipeline=$true,HelpMessage="Module source paths to be processed[-paths 'c:\path-to\','c:\path2-to']")]
         [ValidateScript({Test-Path $_})]
         [string[]]$Paths,
-        [Parameter(HelpMessage="Repository (defaults to `$global:localPsRepo)[-Repository 'somerepo']")]
+        [Parameter(ParameterSetName='Names',Position=0,Mandatory=$false,ValueFromPipeline=$true,HelpMessage="Array of Module names to be searched on the specified Repository[-paths 'c:\path-to\','c:\path2-to']")]
+        [string[]]$Names,
+        [Parameter(HelpMessage="Repository (defaults to `$global:localPsRepo). Can also be set to the UNC path to a Repository.SourceLocation path (for use with PsGet, which lacks PSRepository support)[-Repository 'somerepo']")]
         [string]$Repository=$localPsRepo,
         [Parameter(HelpMessage="Switch to return a summarizing object to the pipeline (defaults true)[-OutObject `$false]")]
         [switch] $OutObject=$true
     ) ;
     BEGIN {
         $verbose = ($VerbosePreference -eq "Continue") ;
-        $rgxNuPkgFileName = '.*\.(\d+\.\d+\.\d+)\.nupkg$' ; 
-        $repoDirs=$allPkgs=@() ; 
-        $RepoSrc = (Get-PSRepository -name $Repository).SourceLocation ; 
-        $allPkgs=@() ; 
-    } ; 
+        $rgxNuPkgFileNames = '.*\.(\d+\.\d+\.\d+)\.nupkg$' ;
+        $repoDirs=$allPkgs=@() ;
+        if($Paths -AND $Names){
+            throw "-Paths and -Names both specified. Please use one or the other!" ;
+            break ;
+        } ;
+        #*======v FUNCTIONS v======
+        if(-not (gcm test-isUNCPath -ea 0)){
+            #*------v Function test-IsUncPath v------
+            function test-IsUncPath {
+                [OutputType('bool')]
+                [CmdletBinding()]
+                PARAM(
+                    [Parameter(Mandatory,Position=0,ValueFromPipeline=$true)]
+                    [ValidateNotNullOrEmpty()]
+                    [string]$Path
+                ) ;
+                PROCESS {
+                    $Error.Clear() ;
+                    foreach($item in $path) {
+                        $PathInfo=[System.Uri]$item ;
+                        if($PathInfo.IsUnc){
+                            $true | write-output ;
+                        } else {
+                            $false | write-output ;
+                        } ;
+                    } ;
+                } ;
+            } ;
+            #*------^ END Function test-IsUncPath ^------
+        } ;
+        #*======^ END FUNCTIONS ^======
+
+        # make it flip a UNC path in the $Repository value, into assumption it's the $SourceLocation of the repository (doesn't use get-psrepository to resolve path, just uses it as passed in).
+        if( (test-IsUncPath -path $Repository) -AND (test-path $Repository) ){
+            $smsg = '-Repository detected to contain a UNC path spec:' ;
+            $smsg += "`nflipping specified Repository into the `$RepoSrc specification" ;
+            write-verbose $smsg ;
+            $RepoSrc = $Repository ;
+        } else {
+            if( -not (get-module -name PowershellGet)){
+                $smsg = "Missing Required module PowershellGet!" ;
+                if($host.version.major -lt 3){
+                    $smsg += "`nPSv2 lacks PowershellGet support" ;
+                    $smsg += "`nspecify the PSRepository.SourceLocation value, for the -Repository parameter"
+                    $smsg += "`nto work around the gap."
+                    $smsg += "`nThen use PSGet's install-module -ModulePath [uncpathto.nupkg] -Global"
+                    $smsg += "`nto install the found highest rev, to the 'Global'/System profile - as Psv2 lacks an AllUsers profile"
+                } ;
+            } else {
+                $RepoSrc = (Get-PSRepository -name $Repository).SourceLocation ;
+            } ;
+        } ;
+        $allPkgs=@() ;
+    } ;
     PROCESS{
-        $procd=0 ; $ttl = ($Paths|measure).count ; 
-        foreach ($path in $Paths){
-            $procd++ ; 
+        $procd=0 ;
+        $srchtargets = @() ;
+        if($Names){
+            $ttl = ($Names | measure).count ;
+            foreach ($name in $Names){
+                $procd++ ;
+                #$srchtargets += "$($reposrc)\$($Name)*.nupkg"
+                $srchtargets += join-path -path $reposrc -childpath "$($Name)*.nupkg"
+            } ;
+        } elseif ($Paths){
+            $ttl = ($Paths|measure).count ;
+            $procd++ ;
+            foreach ($path in $Paths){
+                #$srchtargets += "$($reposrc)\$(split-path $path -leaf)*.nupkg"
+                $srchtargets += join-path -path $reposrc -childpath "$(split-path $path -leaf)*.nupkg"
+            } ;
+        } ;
+        foreach ($path in $srchtargets){
+            $procd++ ;
             TRY {
                 $error.clear() ;
-                write-host "(processing:$($path)...)" ; 
-                $pltGci=[ordered]@{path ="$($reposrc)\$(split-path $path -leaf)*.nupkg" ; ea = 'STOP'} ;
-                write-verbose "`nget-childitem w`n$(($pltGci|out-string).trim())" ; 
-                $pkgs = gci @pltGci ; 
+                write-host "(processing:$($path)...)" ;
+                #$pltGci=[ordered]@{path ="$($reposrc)\$(split-path $path -leaf)*.nupkg" ; ea = 'STOP'} ;
+                $pltGci=[ordered]@{path = $path ; ea = 'STOP'} ;
+                write-verbose "`nget-childitem w`n$(($pltGci|out-string).trim())" ;
+                $pkgs = gci @pltGci ;
                 $pkgs = $pkgs | select *,@{name="version";expression={[version]([regex]::match($_.name,'.*\.(\d+\.\d+\.\d+)\.nupkg$').captures[0].groups[1].value)}} | sort version ;
                 if($pkgs){
                     if($pkgs -is [system.array]){$allPkgs += $pkgs[-1]}
-                    else {$allPkgs += $pkgs} ; 
-                    write-verbose "`n$(($allPkgs[-1]|ft -a name,lastwritetime,version|out-string).trim())" ; 
+                    else {$allPkgs += $pkgs} ;
+                    write-verbose "`n$(($allPkgs[-1]|ft -a name,lastwritetime,version|out-string).trim())" ;
 
-                } ; 
+                } ;
             } CATCH {
                 $ErrTrapd=$Error[0] ;
                 $smsg = "$('*'*5)`nFailed processing $($ErrTrapd.Exception.ItemName). `nError Message: $($ErrTrapd.Exception.Message)`nError Details: `n$(($ErrTrapd|out-string).trim())`n$('-'*5)" ;
-                if ($logging) { Write-Log -LogContent $smsg -Path $logfile -useHost -Level Info } #Error|Warn|Debug 
+                if ($logging) { Write-Log -LogContent $smsg -Path $logfile -useHost -Level Info } #Error|Warn|Debug
                 else{ write-warning "$((get-date).ToString('HH:mm:ss')):$($smsg)" } ;
                 Break  #Opts: STOP(debug)|EXIT(close)|CONTINUE(move on in loop cycle)|BREAK(exit loop iteration)|THROW $_/'CustomMsg'(end script with Err output)
-            } ; 
-        } ; 
+            } ;
+        } ;
     }  # PROC-E
     END {
         if(-not($outObject)){
-            $smsg = "`n$(($allpkgs|ft -a name,lastwritetime,version|out-string).trim())" ; 
-            write-host $smsg ; 
-        } else { 
-          $allpkgs | write-output ; 
-        } ; 
+            $smsg = "`n$(($allpkgs|ft -a name,lastwritetime,version|out-string).trim())" ;
+            write-host $smsg ;
+        } else {
+          $allpkgs | write-output ;
+        } ;
     } ;
 }
 
 #*------^ get-RepoModulesHighestVersion.ps1 ^------
+
 
 #*------v Install-ModulesTin.ps1 v------
 function Install-ModulesTin {
@@ -667,6 +762,7 @@ function Install-ModulesTin {
 
 #*------^ Install-ModulesTin.ps1 ^------
 
+
 #*------v load-Module.ps1 v------
 function load-Module {
     <#
@@ -739,6 +835,7 @@ function load-Module {
 }
 
 #*------^ load-Module.ps1 ^------
+
 
 #*------v load-ModuleFT.ps1 v------
 function load-ModuleFT {
@@ -886,6 +983,7 @@ function load-ModuleFT {
 
 #*------^ load-ModuleFT.ps1 ^------
 
+
 #*------v mount-module.ps1 v------
 function mount-Module {
     <#
@@ -1005,6 +1103,7 @@ function mount-Module {
 
 #*------^ mount-module.ps1 ^------
 
+
 #*------v register-localPSRepository.ps1 v------
 function register-localPSRepository {
     <#
@@ -1093,6 +1192,7 @@ function register-localPSRepository {
 }
 
 #*------^ register-localPSRepository.ps1 ^------
+
 
 #*------v revert-ModuleInstalledRevision.ps1 v------
 function revert-moduleInstalledRevision {
@@ -1210,6 +1310,7 @@ function revert-moduleInstalledRevision {
 }
 
 #*------^ revert-ModuleInstalledRevision.ps1 ^------
+
 
 #*------v test-UnReleasedModuleContent.ps1 v------
 function test-UnReleasedModuleContent {
@@ -1330,6 +1431,7 @@ function test-UnReleasedModuleContent {
 
 #*------^ test-UnReleasedModuleContent.ps1 ^------
 
+
 #*------v Uninstall-AllModules.ps1 v------
 function Uninstall-AllModules {
     <#
@@ -1396,6 +1498,7 @@ https://docs.microsoft.com/en-us/powershell/azure/uninstall-az-ps?view=azps-1.0.
 }
 
 #*------^ Uninstall-AllModules.ps1 ^------
+
 
 #*------v uninstall-ModulesObsolete.ps1 v------
 function uninstall-ModulesObsolete {
@@ -1579,6 +1682,7 @@ function uninstall-ModulesObsolete {
 
 #*------^ uninstall-ModulesObsolete.ps1 ^------
 
+
 #*------v update-PSPowerShellGetLegacy.ps1 v------
 Function update-PSPowerShellGetLegacy {
     <#
@@ -1755,16 +1859,19 @@ Remove the -whatif to run a full execution & install.
 
 #*------^ update-PSPowerShellGetLegacy.ps1 ^------
 
+
 #*======^ END FUNCTIONS ^======
 
-Export-ModuleMember -Function check-ReqMods,Disconnect-PssBroken,find-profileScripts,Get-ModulePublishedVersion,get-RepoModulesHighestVersion,Install-ModulesTin,load-Module,load-ModuleFT,mount-Module,register-localPSRepository,revert-moduleInstalledRevision,test-UnReleasedModuleContent,Uninstall-AllModules,uninstall-ModulesObsolete,update-PSPowerShellGetLegacy -Alias *
+Export-ModuleMember -Function check-ReqMods,Disconnect-PssBroken,find-profileScripts,Get-ModulePublishedVersion,get-RepoModulesHighestVersion,test-IsUncPath,Install-ModulesTin,load-Module,load-ModuleFT,mount-Module,register-localPSRepository,revert-moduleInstalledRevision,test-UnReleasedModuleContent,Uninstall-AllModules,uninstall-ModulesObsolete,update-PSPowerShellGetLegacy -Alias *
+
+
 
 
 # SIG # Begin signature block
 # MIIELgYJKoZIhvcNAQcCoIIEHzCCBBsCAQExCzAJBgUrDgMCGgUAMGkGCisGAQQB
 # gjcCAQSgWzBZMDQGCisGAQQBgjcCAR4wJgIDAQAABBAfzDtgWUsITrck0sYpfvNR
-# AgEAAgEAAgEAAgEAAgEAMCEwCQYFKw4DAhoFAAQUNSzJ59vqb80F/LhNsmXs92X6
-# 3LSgggI4MIICNDCCAaGgAwIBAgIQWsnStFUuSIVNR8uhNSlE6TAJBgUrDgMCHQUA
+# AgEAAgEAAgEAAgEAAgEAMCEwCQYFKw4DAhoFAAQUUJhYA3c8vgjYK2gRLudL++Q+
+# zQKgggI4MIICNDCCAaGgAwIBAgIQWsnStFUuSIVNR8uhNSlE6TAJBgUrDgMCHQUA
 # MCwxKjAoBgNVBAMTIVBvd2VyU2hlbGwgTG9jYWwgQ2VydGlmaWNhdGUgUm9vdDAe
 # Fw0xNDEyMjkxNzA3MzNaFw0zOTEyMzEyMzU5NTlaMBUxEzARBgNVBAMTClRvZGRT
 # ZWxmSUkwgZ8wDQYJKoZIhvcNAQEBBQADgY0AMIGJAoGBALqRVt7uNweTkZZ+16QG
@@ -1779,9 +1886,9 @@ Export-ModuleMember -Function check-ReqMods,Disconnect-PssBroken,find-profileScr
 # AWAwggFcAgEBMEAwLDEqMCgGA1UEAxMhUG93ZXJTaGVsbCBMb2NhbCBDZXJ0aWZp
 # Y2F0ZSBSb290AhBaydK0VS5IhU1Hy6E1KUTpMAkGBSsOAwIaBQCgeDAYBgorBgEE
 # AYI3AgEMMQowCKACgAChAoAAMBkGCSqGSIb3DQEJAzEMBgorBgEEAYI3AgEEMBwG
-# CisGAQQBgjcCAQsxDjAMBgorBgEEAYI3AgEVMCMGCSqGSIb3DQEJBDEWBBRIeNb3
-# J+S2UIU1T/e45pxfqFAxZjANBgkqhkiG9w0BAQEFAASBgJYkNVT6cSpe6LVB53kO
-# EUVtb0jGrQk2g95DsGWDZndY3AsOqjPzTII25WsBjIrXHgvWp4RJqST8tPujzX6f
-# tXdr5gVnwMIm+fyA0WpczZele8WHwfPXGRP4X0MV03kl0g1/YBN2ISGgrWok+c6x
-# dV2HtXx89ACZI5ak1FhTkH5k
+# CisGAQQBgjcCAQsxDjAMBgorBgEEAYI3AgEVMCMGCSqGSIb3DQEJBDEWBBQz+Jmg
+# WKuDvq1jH9ANxR33i/Pu+DANBgkqhkiG9w0BAQEFAASBgGJJpNFigVBbSNuwIaYf
+# VZDz8BQtLXgImpb9c5NTeTbdw+x7GP0XKnbjLRKJPGGcauB300UGKwPolupfnjbM
+# PRQo1u8tGOmZ9DUbPy2gaMzuXL/kvLLyGkyuu3uq5rTOpasAzEsRmgLYVW04tDB6
+# nF6P3mA/gEOpRlHhFxK/FRHA
 # SIG # End signature block
